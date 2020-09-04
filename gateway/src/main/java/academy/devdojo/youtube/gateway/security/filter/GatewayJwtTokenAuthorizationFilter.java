8dcf4c6d70a1de7901d5ef1fa7e493ea2d2c61b4
harisson.nas@gmail.com <harisson.nas@gmail.com>
2020-09-04 03:46:09 -0300

    Swagger Documentation - DevDojo

--- gateway/src/main/java/academy/devdojo/youtube/gateway/security/filter/GatewayJwtTokenAuthorizationFilter.java
+++ gateway/src/main/java/academy/devdojo/youtube/gateway/security/filter/GatewayJwtTokenAuthorizationFilter.java
@@ -3,20 +3,16 @@ package academy.devdojo.youtube.gateway.security.filter;
 import academy.devdojo.youtube.core.property.JwtConfiguration;
 import academy.devdojo.youtube.security.filter.JwtTokenAuthorizationFilter;
 import academy.devdojo.youtube.security.token.converter.TokenConverter;
-import academy.devdojo.youtube.security.util.SecurityContextUtil;
 import com.netflix.zuul.context.RequestContext;
 import com.nimbusds.jwt.SignedJWT;
 import lombok.SneakyThrows;
 import org.springframework.lang.NonNull;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-import static academy.devdojo.youtube.security.util.SecurityContextUtil.*;
-import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
+import static academy.devdojo.youtube.security.util.SecurityContextUtil.setSecurityContext;
 
 public class GatewayJwtTokenAuthorizationFilter extends JwtTokenAuthorizationFilter {
     public GatewayJwtTokenAuthorizationFilter(JwtConfiguration jwtConfiguration, TokenConverter tokenConverter) {
@@ -26,7 +22,7 @@ public class GatewayJwtTokenAuthorizationFilter extends JwtTokenAuthorizationFil
     @Override
     @SneakyThrows
     @SuppressWarnings("Duplicates")
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) {
         String header = request.getHeader(jwtConfiguration.getHeader().getName());
 
         if (header == null || !header.startsWith(jwtConfiguration.getHeader().getPrefix())){
